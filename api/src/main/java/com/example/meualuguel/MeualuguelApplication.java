c3b285483a7705b623491b5d5883ad1d7d2fd628
Diogo Cardoso <diogomescardoso@gmail.com>
2024-02-17 22:21:50 -0300

    inicialização do projeto

---
+++ api/src/main/java/com/example/meualuguel/MeualuguelApplication.java
@@ -0,0 +1,13 @@
+package com.example.meualuguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeualuguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeualuguelApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/meualuguel/MeualuguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meualuguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeualuguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
